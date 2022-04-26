56905f56e64e0bdb8009fa56c5bbd46857b6c936
Roberta <63109114+rmftelier@users.noreply.github.com>
2022-04-26 22:17:29 +0000

    👩‍💻 Configurações e DI (Aula 18/04)

--- SnippetBox/cmd/web/handlers.go
+++ SnippetBox/cmd/web/handlers.go
@@ -4,40 +4,43 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(rw http.ResponseWriter, r *http.Request){
+//A parte (app *application) significa que a função home pertence a application
+func(app *application) home(rw http.ResponseWriter, r *http.Request){
   //Teste para verificar se um caminho está ou não no código, caso não esteja será enviado ao usuário um erro
   if r.URL.Path != "/"{
     http.NotFound(rw, r)
     return
   } 
-
+ 
   //Adicionado dia 13/04
   files := []string{
     "./ui/html/home.page.tmpl.html",
     "./ui/html/base.layout.tmpl.html",
     "./ui/html/footer.partial.tmpl.html",
   }
+  
   ts, err := template.ParseFiles(files...)
   if err != nil{
-    log.Println(err.Error())
+    app.errorLog.Println(err.Error())
     http.Error(rw, "Internal Error", 500)
     return
   }
+  
   err = ts.Execute(rw, nil)
   if err != nil{
-    log.Println(err.Error())
+    app.errorLog.Println(err.Error())
     http.Error(rw, "Internal Error", 500)
     return
-  }
+  }  
 }
 
+
 //http://localhost:4000/snippet?id=123
-func showSnippet(rw http.ResponseWriter, r *http.Request){
+func(app *application) showSnippet(rw http.ResponseWriter, r *http.Request){
 
   //Pegamos o id da URL e convertemos de string para int e já tratamos possíveis erros no back-end
   id,err := strconv.Atoi(r.URL.Query().Get("id"))
@@ -49,7 +52,7 @@ func showSnippet(rw http.ResponseWriter, r *http.Request){
   fmt.Fprintf(rw, "Exibir o Snippet de ID: %d", id)
 }
 
-func createSnippet(rw http.ResponseWriter, r *http.Request){
+func(app *application) createSnippet(rw http.ResponseWriter, r *http.Request){
   if r.Method != "POST"{
     //Tem que ser colocado acima dos códigos WriteHeader e http.Error
     rw.Header().Set("Allow","POST")
--- SnippetBox/cmd/web/main.go
+++ SnippetBox/cmd/web/main.go
@@ -2,27 +2,47 @@
 package main
 
 import (
+  "flag"
 	"log"
 	"net/http"
+  "os"
 )
 
+//Criação do struct
+type application struct{
+    errorLog *log.Logger
+    infoLog *log.Logger
+}
+
 func main() {
+   //Flag 
+  addr := flag.String("addr", ":4000", "Porta da Rede")
+  flag.Parse()
+
+  infoLog := log.New(os.Stdout, "INFO:\t", log.Ldate|log.Ltime)
+  errorLog := log.New(os.Stderr, "ERRO:\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+   app := &application{
+        errorLog: errorLog,
+        infoLog: infoLog,
+   }
+
+  
 	//Conexão ao servidor:
   mux := http.NewServeMux()
 
   //Caminho relativo:
-  mux.HandleFunc("/", home)
+  mux.HandleFunc("/", app.home)
 
   //Caminhos absolutos:
-  mux.HandleFunc("/snippet", showSnippet)
-  mux.HandleFunc("/snippet/create", createSnippet)
+  mux.HandleFunc("/snippet", app.showSnippet)
+  mux.HandleFunc("/snippet/create", app.createSnippet)
 
   //Criação de um manipulador (handler) de servidor de arquivos (file server)
   fileServer := http.FileServer(http.Dir("./ui/static/"))
   mux.Handle("/static/",http.StripPrefix("/static",fileServer))
   
-  log.Println("Inicializando o servidor na porta: 4000")
-  //Caso ocorra erro:
-  err := http.ListenAndServe(":4000", mux)
-  log.Fatal(err)
+   infoLog.Printf("Inicializando o servidor na porta %s\n", *addr)
+   err := http.ListenAndServe(*addr, mux)
+   errorLog.Fatal(err)
 }
